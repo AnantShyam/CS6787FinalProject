05214ce2c7e19170ad412b1016f6717647216fc7
Anant <anant@admins-mbp.myfiosgateway.com>
2024-03-30 15:43:10 -0400

    add some dataset reading code

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import torch
+import sklearn 
+import helpers
+            
+
+def newton_method():
+    """
+    Standard implementation of Newton's Method
+    """
+    pass
+
+
+if __name__ == "__main__":
+    a9a_dataset, labels = helpers.read_a9a_dataset('data/a9a_train.txt')
+    
