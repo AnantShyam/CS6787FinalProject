59184fd46ff003abb5d41041bd9fd6eb88888b10
Anant <anant@admins-mbp.myfiosgateway.com>
2024-03-31 18:28:54 -0400

    add newton's method loss curve for a9a dataset

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch
 import sklearn 
 import helpers
 from tqdm import tqdm
+import matplotlib.pyplot as plt
             
 class NewtonMethod:
 
@@ -16,14 +17,11 @@ class NewtonMethod:
         for i in range(n):
             training_example, label = self.X[:, i], self.y[i]
             loss = loss + (torch.log(1 + torch.exp((-w.T @ training_example) * label)))
-        # print(loss)
-        #print(type(loss))
-        regularization_constant = 0.1
+
+        regularization_constant = 0.05
         regularization_term = (regularization_constant * torch.pow(torch.norm(w), 2)/2.0)
-        #print(type(regularization_term))
         total_loss = (loss/n) + regularization_term
         return total_loss
-        #return torch.tensor([total_loss])
 
 
     def newton_method(self):
@@ -32,23 +30,28 @@ class NewtonMethod:
         Using L2-regularized logistic regression (trying to get a baseline)
         """
         w = torch.rand(self.X.shape[0])
-        print(self.l2_regularized_logistic_regression_loss(w))
 
-        for i in tqdm(range(10)):
+        loss_values = {}
+
+        for epoch in tqdm(range(8)):
             hessian_matrix = torch.func.hessian(self.l2_regularized_logistic_regression_loss)(w)
             gradient = torch.autograd.functional.jacobian(self.l2_regularized_logistic_regression_loss, w)
             hessian_inverse = torch.inverse(hessian_matrix)
             w = w - (0.5 * (hessian_inverse @ gradient))
-            print(self.l2_regularized_logistic_regression_loss(w))
+            loss_values[epoch + 1] = self.l2_regularized_logistic_regression_loss(w).item()
 
+        return w, loss_values
 
-        return w
 
 
 if __name__ == "__main__":
     a9a_dataset, labels = helpers.read_a9a_dataset('data/a9a_train.txt')
-    #print(a9a_dataset)
     newton_m = NewtonMethod(a9a_dataset, labels)
-    new_w  = newton_m.newton_method()
-    # print(new_w)
+    new_w, loss_values  = newton_m.newton_method()
     
+    print(loss_values)
+    plt.plot([key for key in loss_values], [loss_values[key] for key in loss_values])
+    plt.xlabel("Epoch Number")
+    plt.ylabel("Loss")
+    plt.title("Loss vs Epoch Number (Standard Newton's Method on a9a dataset)")
+    plt.show()
