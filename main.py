1cf4a38a6c5a7c4b89662d7a969c21cdf8421087
Anant <anant@admins-mbp.myfiosgateway.com>
2024-04-01 13:22:00 -0400

    add gradient descent code for a9a dataset

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import helpers
 from tqdm import tqdm
 import matplotlib.pyplot as plt
             
-class NewtonMethod:
+class A9A_Analysis:
 
     def __init__(self, X, y):
         self.X = X 
@@ -22,6 +22,25 @@ class NewtonMethod:
         regularization_term = (regularization_constant * torch.pow(torch.norm(w), 2)/2.0)
         total_loss = (loss/n) + regularization_term
         return total_loss
+
+    # H = U (Sigma) V^T 
+    # H = A B, A = first column of U, B = first column of V^T
+
+    def gradient_descent(self):
+        w = torch.rand(self.X.shape[0])
+        alpha = 1 # choose some small step size to get as close to minimum possible loss as possible
+        loss_values = []
+        num_epochs = 100
+        for epoch in tqdm(range(num_epochs)):
+            gradient = torch.autograd.functional.jacobian(self.l2_regularized_logistic_regression_loss, w)
+            w = w - (alpha * gradient)
+            loss_values.append(self.l2_regularized_logistic_regression_loss(w).item())
+        print(loss_values[-1])
+        plt.plot([i for i in range(1, num_epochs + 1)], loss_values)
+        plt.xlabel("Epoch Number")
+        plt.ylabel("Loss")
+        plt.show()
+        
 
 
     def newton_method(self):
@@ -46,12 +65,15 @@ class NewtonMethod:
 
 if __name__ == "__main__":
     a9a_dataset, labels = helpers.read_a9a_dataset('data/a9a_train.txt')
-    newton_m = NewtonMethod(a9a_dataset, labels)
-    new_w, loss_values  = newton_m.newton_method()
+    a9a = A9A_Analysis(a9a_dataset, labels)
     
-    print(loss_values)
-    plt.plot([key for key in loss_values], [loss_values[key] for key in loss_values])
-    plt.xlabel("Epoch Number")
-    plt.ylabel("Loss")
-    plt.title("Loss vs Epoch Number (Standard Newton's Method on a9a dataset)")
-    plt.show()
+    a9a.gradient_descent()
+    
+    # new_w, loss_values  = newton_m.newton_method()
+    
+    # print(loss_values)
+    # plt.plot([key for key in loss_values], [loss_values[key] for key in loss_values])
+    # plt.xlabel("Epoch Number")
+    # plt.ylabel("Loss")
+    # plt.title("Loss vs Epoch Number (Standard Newton's Method on a9a dataset)")
+    # plt.show()
