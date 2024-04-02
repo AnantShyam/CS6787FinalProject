092333091f17d60ccc76535e5b0316b83e26cd99
Anant <anant@admins-mbp.myfiosgateway.com>
2024-04-02 14:34:28 -0400

    add file for transforming hessian matrix

---
+++ newton_accelerated.py
@@ -0,0 +1,17 @@
+import numpy as np
+import torch 
+import helpers
+
+class Newton_Accelerated:
+
+    def __init__(self, hessian):
+        self.hessian = hessian # untransformed hessian 
+
+    def reshape_hessian(self, hessian):
+        pass
+     
+
+
+if __name__ == "__main__":
+    pass
+
