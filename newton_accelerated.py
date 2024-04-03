2721aea4349f713570867031330ed0d16a47593e
Anant <anant@admins-mbp.myfiosgateway.com>
2024-04-02 22:42:36 -0400

    clean newton_accelerated class up a bit

--- newton_accelerated.py
+++ newton_accelerated.py
@@ -4,17 +4,19 @@ import helpers
 
 class Newton_Accelerated:
 
-    def __init__(self, hessian):
+    def __init__(self, hessian, m, n):
         self.hessian = hessian # untransformed hessian 
+        self.m = m 
+        self.n = n
 
-    def reshape_hessian(self, hessian, m, n):
+    def reshape_hessian(self):
         # hessian is of dimension mn x mn
-        hessian_shape = hessian.shape
-        assert m * n == hessian_shape[0]
+        hessian_shape = self.hessian.shape
+        assert self.m * self.n == hessian_shape[0]
         assert len(hessian_shape) == 2 and hessian_shape[0] == hessian_shape[1] # assert that hessian is a square matrix
         
-        four_dim_hessian = torch.reshape(hessian, (m, n, m, n))
-        non_symmetric_hessian = torch.reshape(four_dim_hessian, (m ** 2, n ** 2))
+        four_dim_hessian = torch.reshape(self.hessian, (self.m, self.n, self.m, self.n))
+        non_symmetric_hessian = torch.reshape(four_dim_hessian, (self.m ** 2, self.n ** 2))
         return non_symmetric_hessian
 
 
