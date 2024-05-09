720c3e48940cfcd6c4d88d68378c873fd2612f2c
Anant <anant@dhcp-vl2041-27170.eduroam.cornell.edu>
2024-05-09 16:16:46 -0400

    remove bias from model layers

--- nonconvex_model/cifar_model.py
+++ nonconvex_model/cifar_model.py
@@ -9,13 +9,13 @@ class CIFAR10Net(nn.Module):
     def __init__(self):
         
         super(CIFAR10Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.dropout1 = nn.Dropout(0.25)
-        self.fc1 = nn.Linear(64 * 16 * 16, 512)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(512, 10)
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1, bias=False)
+        self.pool = nn.MaxPool2d(2, 2, bias=False)
+        self.dropout1 = nn.Dropout(0.25, bias=False)
+        self.fc1 = nn.Linear(64 * 16 * 16, 512, bias=False)
+        self.dropout2 = nn.Dropout(0.5, bias=False)
+        self.fc2 = nn.Linear(512, 10, bias=False)
         self.initialize_weights()
 
     def forward(self, x):
